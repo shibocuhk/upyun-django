a3ace4130c3a188500863a5eff2d577dd142b5af
Bruce Shi <shibocuhk@gmail.com>
2015-09-12 20:58:37 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='upyun-django-storage',
+    version='0.0.1',
+    packages=['upyun-django-storage'],
+    url='http://bruceshi.me',
+    license='MIT',
+    author='bruceshi',
+    author_email='shibocuhk@gmail.com',
+    description='django upyun storage'
+)

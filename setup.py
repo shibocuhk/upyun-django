18b661bbc107eafb7a2a941842ed87cd45cce7d1
Bruce Shi <shibocuhk@gmail.com>
2015-09-12 21:47:33 +0800

    * change package name * add more classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name='upyun-django-storage',
+    name='upyun-django',
     version='0.0.1',
-    packages=['upyun-django-storage'],
+    packages=['upyun-django'],
     url='http://bruceshi.me',
     license='MIT',
     author='bruceshi',
     author_email='shibocuhk@gmail.com',
-    description='django upyun storage'
+    description='django upyun support',
+    install_requires=[
+        'upyun',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+    ],
 )
